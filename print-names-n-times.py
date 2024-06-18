a822d8867e0cc458232f9723b4f6a0025fa86444
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-06-18 17:26:16 +0530

    print names using recursion

---
+++ print-names-n-times.py
@@ -0,0 +1,21 @@
+class Solution:
+    def printGfg(self, n):
+        # Code here
+        if n<=0:
+            return
+        self.printGfg(n-1)
+        print('GFG', end=" ")
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printGfg(N)
+        print()
+# } Driver Code Ends
